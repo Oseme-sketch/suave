eaab69bb85997192acf245ee05b06301b3301882
Osemekhian Omoifo <oiomoifo@gmail.com>
2024-01-14 08:14:10 +0100

    chore: configure vite library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "Suave",
+      fileName: "suave",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+});
